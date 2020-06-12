9ffc6f524976992c86e27094b0ef9236d3893d2c
nashrul-8 <62601331+nashrul-8@users.noreply.github.com>
2020-06-12 22:51:23 +0700

    manage file and fixed error

---
+++ src/lexer.py
@@ -0,0 +1,36 @@
+from sly import Lexer
+
+
+class BasicLexer(Lexer):
+    tokens = {NAME, NUMBER, STRING, IF, THEN,
+              ELSE, FOR, FUN, TO, ARROW, EQEQ, PRINT}
+    ignore = '\t '
+
+    literals = {'=', '+', '-', '/', '*', '(', ')', ',', ';'}
+
+    # Mendefinisikan token
+    IF = r'JIKA'
+    THEN = r'MAKA'
+    ELSE = r'LAINNYA'
+    FOR = r'UNTUK'
+    FUN = r'FUNGSI'
+    TO = r'KE'
+    PRINT = r'CETAK'
+    ARROW = r'->'
+    NAME = r'[A-Za-z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+
+    EQEQ = r'=='
+
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    @_(r'#.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
