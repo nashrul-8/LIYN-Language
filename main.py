78987202d726d8b5f622f47a38a70ef36fbbf545
nashrul-8 <62601331+nashrul-8@users.noreply.github.com>
2020-06-12 22:49:22 +0700

    fixed error

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import liyn_lexer
-import liyn_parser
-import liyn_interpreter
+from src.lexer import BasicLexer
+from src.parser import BasicParser
+from src.interpreter import BasicExecute
+from sys import *
+
+
+def open_file(filename):
+    data = open(filename, "r").read()
+    return data
+
 
 if __name__ == '__main__':
-	lexer = liyn_lexer.BasicLexer()
-	parser = liyn_parser.BasicParser()
-	env = {}
-	while True :
-		try :
-			text = input('liyn > ')
-			 except EOFError:
-            break
-        if text:
-            tree = parser.parse(lexer.tokenize(text))
-            liyn_interpreter.BasicExecute(tree, env)
+    lexer = BasicLexer()
+    parser = BasicParser()
+    #interpreter = basicinterpreter
+    env = {}
+    if len(argv) > 1:
+        data = open(argv[1])
+        text = data.readlines()
+        for line in text:
+            #lex = lexer.tokenize(line)
+            tree = parser.parse(lexer.tokenize(line))
+            BasicExecute(tree, env)
+    else:
+        while True:
+            try:
+                text = input('liyn_language » ')
+            except EOFError:
+                break
+            if text:
+                tree = parser.parse(lexer.tokenize(text))
+                BasicExecute(tree, env)
